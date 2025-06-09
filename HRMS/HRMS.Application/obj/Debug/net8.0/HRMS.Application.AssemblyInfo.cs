1fbed929344e79767db8891db370aceeac0c7a7b
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-10 00:19:09 +0545

    some chnages

--- HRMS/HRMS.Application/obj/Debug/net8.0/HRMS.Application.AssemblyInfo.cs
+++ HRMS/HRMS.Application/obj/Debug/net8.0/HRMS.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2c758087b906fc210fd9efc6571da542fca1b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae646defb81e7bc31c5f0f4c02e15ad5b7f4c078")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
