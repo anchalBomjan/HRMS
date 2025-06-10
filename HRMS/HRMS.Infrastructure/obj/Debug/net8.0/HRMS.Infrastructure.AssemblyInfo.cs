9ccf487868e0d1a70c0c489e645e9a8ab7b6d081
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-10 15:34:49 +0545

    some changes

--- HRMS/HRMS.Infrastructure/obj/Debug/net8.0/HRMS.Infrastructure.AssemblyInfo.cs
+++ HRMS/HRMS.Infrastructure/obj/Debug/net8.0/HRMS.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf664669fb571ba8cfe9d7fa992f160e8e9dcdfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab639c39ca88c02948b25c58fec70c55db150f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
