80676518fc33687e1f240a3e22aad110f0289413
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-11 13:31:12 +0545

    Create RoleController and implement its necessary function on it

---
+++ HRMS/HRMS.API/Controllers/RoleController.cs
@@ -0,0 +1,76 @@
+﻿using HRMS.Application.Commands.Role.Create;
+using HRMS.Application.Commands.Role.Delete;
+using HRMS.Application.Commands.Role.Update;
+using HRMS.Application.DTOs;
+using HRMS.Application.Queries.Role.GetRoleByIdQuery;
+using HRMS.Application.Queries.Role.GetRoleQuery;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public RoleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Create")]
+        [ProducesDefaultResponseType(typeof(int))]
+
+        public async Task<IActionResult> CreateRoleAsync(RoleCreateCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpGet("GetAll")]
+        [ProducesDefaultResponseType(typeof(List<RoleResponseDTO>))]
+        public async Task<IActionResult> GetRoleAsync()
+        {
+             return Ok(await _mediator.Send(new GetRoleQuery()));
+
+        }
+        [HttpGet("{id}")]
+        [ProducesDefaultResponseType(typeof(List<RoleResponseDTO>))]
+        public async Task<IActionResult> GetRoleByIdAsync(string id)
+        {
+            return Ok(await _mediator.Send(new GetRoleByIdQuery() { RoleId=id }));
+
+        }
+
+        [HttpDelete("Delete/{id}")]
+        [ProducesDefaultResponseType(typeof(int))]
+
+        public async Task<IActionResult> DeleteRoleAsync(string id)
+        {
+            return Ok(await _mediator.Send(new DeleteRoleCommand() 
+            {
+                RoleId=id 
+            }));
+        }
+
+        [HttpPut("Edit/{id}")]
+        [ProducesDefaultResponseType(typeof(int))]
+
+        public async Task<IActionResult> EditRole(string id, [FromBody] UpdateRoleCommand command)
+        {
+            if(id==command.Id)
+            {
+                var result = await _mediator.Send(command);
+                return Ok(result);
+
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+    }
+}
--- HRMS/HRMS.API/obj/Debug/net8.0/HRMS.API.AssemblyInfo.cs
+++ HRMS/HRMS.API/obj/Debug/net8.0/HRMS.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ded8043273f74e20ae5dce08c4cd02e15bd557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f988dc7581ca2f21f48a5aab219a349be7112c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
