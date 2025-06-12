a560090b61e8b2f8b8ca3b202901b6e490bd4be2
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-12 18:01:48 +0545

    some changes

--- HRMS/HRMS.Application/Queries/employee/GetEmployeeStockAssignmentQuery/GetEmployeeStockAssignmentQueryhandler.cs
+++ HRMS/HRMS.Application/Queries/employee/GetEmployeeStockAssignmentQuery/GetEmployeeStockAssignmentQueryhandler.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Azure.Core;
+using HRMS.Application.Common.Interfaces;
+using HRMS.Application.Queries.employee.GetEmployeeStockAssignmentQuery.ViewModel;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HRMS.Application.Queries.employee.GetEmployeeStockAssignmentQuery
 {
-    internal class GetEmployeeStockAssignmentQueryhandler
+    public  class GetEmployeeStockAssignmentQueryhandler:IRequestHandler<GetEmployeeStockAssignmentQuery,List<EmployeeStockAssignmentViewModel>>
     {
+
+        private readonly IApplicationDbContext _context;
+        public GetEmployeeStockAssignmentQueryhandler(IApplicationDbContext context)
+        {
+            _context = context;
+            
+        }
+
+        public async Task<List<EmployeeStockAssignmentViewModel>> Handle(GetEmployeeStockAssignmentQuery query, CancellationToken ct)
+        {
+            var assignments = await _context.StockAssignments
+              .Where(sa => sa.EmployeeId == query.EmployeeId)
+               .Select(sa => new EmployeeStockAssignmentViewModel
+                 {
+                   EmployeeId = sa.Employee.Id,
+                   EmployeeName = sa.Employee.Name,
+                  AssignmentDate = sa.AssigmentDate,
+                  AssignedQuantity = sa.AsssignedQuantity,
+                  StockName = sa.Stock.Name,
+                  StockTotalQuantity = sa.Stock.Quantity
+               })
+                .ToListAsync(ct);
+
+            return assignments;
+
+        }
     }
 }
