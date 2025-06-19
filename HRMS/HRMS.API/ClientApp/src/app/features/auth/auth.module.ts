18a5064465674138da681fff2d7149c0b1b7f9ac
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-19 12:16:18 +0545

    adding authmodel. and authroutingmodule

--- HRMS/HRMS.API/ClientApp/src/app/features/auth/auth.module.ts
+++ HRMS/HRMS.API/ClientApp/src/app/features/auth/auth.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'primeng/api';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegistrationComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    SharedModule,
     AuthRoutingModule
   ]
 })
