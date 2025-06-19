e17ddf1524bdfba9cafbe768a2b82b5f97bec851
anchalBomjan <anchal.percoidit@gmail.com>
2025-06-19 14:25:49 +0545

    adding provider in core.module.ts

--- HRMS/HRMS.API/ClientApp/src/app/core/core.module.ts
+++ HRMS/HRMS.API/ClientApp/src/app/core/core.module.ts
@@ -1,12 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { LoggerService } from './services/logger.service';
+import { authGuard } from './guards/auth.guard';
+import { roleGuard } from './guards/role.guard';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,HttpClientModule
+  ],
+
+  providers:[
+    AuthService,
+    LoggerService,
+    {provide:'authGuard',useValue:authGuard},
+    {provide:'roleGauard',useValue:roleGuard},
+    {provide:'HTTP_INTERCEPTORS', useClass:AuthInterceptor,multi:true}
+
+    
+  
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+  constructor(@Optional() @SkipSelf()parentModule:CoreModule) {
+    if(parentModule){
+      throw new Error(
+ 
+        'CoreModule is already loaded. Impoort it in the AppModule only'
+      );
+
+    }
+   
+    
+  }
+}
--- HRMS/HRMS.API/ClientApp/src/app/core/guards/auth.guard.ts
+++ HRMS/HRMS.API/ClientApp/src/app/core/guards/auth.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService); // inject AuthService
+  const router = inject(Router);           // inject Router
+  const expectedRole = route.data['role']; // get expected role from route data
+  const userRole = authService.getUserRole(); // get user role from AuthService
+
+  if (authService.isLoggedIn() && userRole === expectedRole) {
+    return true;  // allow access if logged in and role matches
+  }
+
+  router.navigate(['login']); // otherwise redirect to login
+  return false;
 };
