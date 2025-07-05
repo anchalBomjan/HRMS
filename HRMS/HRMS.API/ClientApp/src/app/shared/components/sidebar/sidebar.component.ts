d66fd91680b1766af7f89f7b251e073b3a02e2e7
anchalBomjan <anchal.percoidit@gmail.com>
2025-07-06 01:25:19 +0545

    somechanges

--- HRMS/HRMS.API/ClientApp/src/app/shared/components/sidebar/sidebar.component.ts
+++ HRMS/HRMS.API/ClientApp/src/app/shared/components/sidebar/sidebar.component.ts
@@ -38,6 +38,11 @@ export class SidebarComponent {
           { label: 'Employees', route: '/home/app-dashboard/employees' },
 
           {label:'Stock',route:'/home/app-dashboard/stocks'},
+          {label:'AssignStock',route:'/home/app-dashboard/Assignstocks'},
+          {label:'Manages User',route:'/home/app-dashboard/ManagesUser'},
+          // {label:'Stock',route:'/home/app-dashboard/stocks'},
+
+
           { label: 'Leaves', route: '/hr/leaves' }
         ];
         break;
